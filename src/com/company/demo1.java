75adb30cc722c7f6e10b2a28d1fa484f489e4997
fengxudong2914 <vanish2914@163.com>
2023-02-11 17:37:52 +0800

    console新增输出语句。

--- src/com/company/demo1.java
+++ src/com/company/demo1.java
@@ -3,5 +3,7 @@ package com.company;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("Hello  World!!h");
+        int num = 2;
+        System.out.println(num + 1);
     }
 }
